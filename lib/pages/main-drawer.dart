28757c89f9e4408df6f2f44785583e12cf529ad8
Michael <7471919@naver.com>
2022-04-29 13:22:02 +0900

    style: drawer ui 제작

--- lib/pages/main-drawer.dart
+++ lib/pages/main-drawer.dart
@@ -20,6 +20,7 @@ class MainDrawer extends StatelessWidget {
   }
 }
 
+//drawer의 윗부분
 class InfoPart extends StatelessWidget {
   const InfoPart({ Key? key }) : super(key: key);
 
@@ -28,17 +29,72 @@ class InfoPart extends StatelessWidget {
     return Container(
       height: 200,
       color: Colors.green,
+      child: Column(
+        children: [
+          //상단바
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () => {
+                  Navigator.of(context).pop(null),
+                },
+              ),
+            ),
+          ),
+          //body부분
+          Flexible(
+            flex: 3,
+            child: Container(
+              child: Text('가로쓰레기통 알리미',
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
+//drawer의 아랫부분
 class MenuPart extends StatelessWidget {
   const MenuPart({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Expanded(
+      child: Container(
+        child: ListView(
+          children: [
+            ListTile(title: Text("쓰레기통 추가신청"),),
+            ListTile(title: Text("쓰레기통 철거신청"),),
+            ListTile(title: Text("쓰레기통 청소신청"),),
+          ],
+        ),
+      ),
     );
   }
 }
+
+// class MainDrawerMenu extends StatelessWidget {
+//   MainDrawerMenu({required TrashManageWidget trashManageWidget}):
+//     this.trashManageWidget = trashManageWidget;
+      
+//   //쓰레기통관리 위젯
+//   TrashManageWidget trashManageWidget;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListTile(
+//       title: trashManageWidget.getTitle(),
+//       onTap: () => {
+
+//       },
+//     );
+//   }
+// }

4c0d7dc610b37941f2aa96ae65053ec7abc489f7
김진용 <imagine@gimjin-yong-ui-MacBookAir.local>
2022-05-13 11:25:58 +0900

    쓰레기통 관리위젯 ui 수정

--- lib/widgets/trash-manage.dart
+++ lib/widgets/trash-manage.dart
@@ -12,7 +12,6 @@ class TrashManageWidget extends StatelessWidget {
         child: ListView(
           children: [
             TrashAddWidget(),
-            TrashDeleteWidget(),
             TrashCleanWidget(),
           ],
         ),
@@ -44,7 +43,7 @@ class _TrashAddWidgetState extends State<TrashAddWidget> {
           context: context,
           builder: (BuildContext context) => AlertDialog(
             title: Text('쓰레기통 추가신청 이유'),
-            content: DropdownButton<String>(
+            content: DropdownButtonFormField<String>(
               value: selectedAddReason,
               items: addReasons
                   .map((item) => DropdownMenuItem<String>(
@@ -52,9 +51,14 @@ class _TrashAddWidgetState extends State<TrashAddWidget> {
                         child: Text(item, style: TextStyle(fontSize: 16)),
                       ))
                   .toList(),
-              onChanged: (item) {
+              onChanged: (String? newValue) {
                 setState(() {
-                  selectedAddReason = item;
+                  selectedAddReason = newValue;
+                });
+              },
+              onSaved: (String? newValue) {
+                setState(() {
+                  selectedAddReason = newValue;
                 });
               },
             ),
@@ -72,61 +76,6 @@ class _TrashAddWidgetState extends State<TrashAddWidget> {
         );
       },
       title: Text("쓰레기통 추가신청"),
-    );
-  }
-}
-
-// 쓰레기통 철거신청 위젯
-class TrashDeleteWidget extends StatefulWidget {
-  @override
-  _TrashDeleteWidgetState createState() => _TrashDeleteWidgetState();
-}
-
-class _TrashDeleteWidgetState extends State<TrashDeleteWidget> {
-  final List<String> deleteReasons = [
-    "1. 도시 미관을 해쳐서",
-    "2. 쓰레기통이 냄새가 심해서",
-    "3. 기타",
-  ];
-
-  String? selectedDeleteReason = '1. 도시 미관을 해쳐서';
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) => AlertDialog(
-            title: Text('쓰레기통 철거신청 이유'),
-            content: DropdownButton<String>(
-              value: selectedDeleteReason,
-              items: deleteReasons
-                  .map((item) => DropdownMenuItem<String>(
-                        value: item,
-                        child: Text(item, style: TextStyle(fontSize: 16)),
-                      ))
-                  .toList(),
-              onChanged: (item) {
-                setState(() {
-                  selectedDeleteReason = item;
-                });
-              },
-            ),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: Text('신청'),
-              ),
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: Text('취소'),
-              ),
-            ],
-          ),
-        );
-      },
-      title: Text("쓰레기통 철거신청"),
     );
   }
 }

e747a7a2bad22a14b3b37a9ed13b5284382f545b
imagine97kim <imagine97kim@gmail.com>
2022-07-26 09:51:07 +0900

    fix : main.dart 파일 패키지 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:client/pages/main-map.dart';
+import 'package:client/pages/main-page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/route_manager.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
-  
+
   runApp(MyApp());
 }
 
